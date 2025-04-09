2347e385ac5be76545dbca2fa25c0fe59f41d28d
oda <odaneo@outlook.com>
2025-04-09 22:49:16 +0900

    11.3

---
+++ learn-c-sharp/Controllers/AuthenticateController.cs
@@ -0,0 +1,48 @@
+﻿using learn_c_sharp.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace learn_c_sharp.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult login([FromBody] LoginDto loginDto)
+        {
+            var signingAlgorithm = SecurityAlgorithms.HmacSha256;
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,"fake_user_id")
+            };
+            var secretByte = Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]!);
+            var signingKey = new SymmetricSecurityKey(secretByte);
+            var signingCredentials = new SigningCredentials(signingKey, signingAlgorithm);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"],
+                claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials
+            );
+
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(tokenStr);
+        }
+    }
+}
